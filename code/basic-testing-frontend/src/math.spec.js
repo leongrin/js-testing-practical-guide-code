5f9bf234c49d0442fc7234a68b0623f7ab12e053
leongrin <ceo@brmed.com>
2022-05-10 17:59:40 -0300

    first commit

---
+++ code/basic-testing-frontend/src/math.spec.js
@@ -0,0 +1,7 @@
+import { it, expect } from 'vitest';
+import { add } from './math';
+
+it('should sum all numbers in an array', function () {
+    const result = add([1, 2, 3]);
+    expect(result).to
+}); 
